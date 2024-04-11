ceee8d80e2f91dfb296760ccae63f8292dc4c14e
Ajay Upadhyay <57ajay.u@gmail.com>
2024-04-11 18:38:10 +0530

    Added Alert in phoneBook to prevent duplicate names

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -7,12 +7,33 @@ const  Phonebook= ()=>{
     ]);
     
     const [newName, setNewName] = useState("");
+
+    const addPerson = (event) => {
+      event.preventDefault();
+      const personObject = {
+        name: newName,
+      };
+      if (persons.some((person) => person.name === newName)) {
+        alert(`${newName} is already added to phonebook`);
+        setNewName("");
+        return;
+      } else {
+        setPersons(persons.concat(personObject));
+        setNewName("");
+      }
+    };
+    
+
+    const handleChange = (event) => {
+        setNewName(event.target.value);
+    }
+
     return(
         <div className="phonebook-container">
             <h2 className="phonebook-title">Phonebook</h2>
-            <form>
+            <form onSubmit={addPerson}>
                 <div>
-                    name: <input />
+                    name: <input onChange={handleChange} type="text"/>
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -21,7 +42,7 @@ const  Phonebook= ()=>{
             <h2 className="phonebook-title">Numbers</h2>
             <div>
                 {persons.map(person => (
-                    <p key={person.name}>{person.name}</p>
+                    <p key={persons.indexOf(person)}>{person.name}</p>
                 ))}
             </div>
         </div>
