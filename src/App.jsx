fd543ac657d6f090b15e3b833edebc1ab08ea159
Ajay Upadhyay <57ajay.u@gmail.com>
2024-04-14 15:17:54 +0530

    Deleted Unnecessary Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Course from "./components/Course";
+
 import Phonebook from "./components/Phonebook";
 const App = ()=>{
 
   return(
     <>
-      <Course />
       <Phonebook />
     </>
   );
